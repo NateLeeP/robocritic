2f1063c9da06e8599cb9b2c3bea17da48006aa02
Nate Pruitt <nlpruitt@gmail.com>
2023-11-22 16:10:28 -0600

    Add helper functions for accessing DB. Add notebook for testing scraping of IGN

---
+++ helper_functions/db_connection.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+from mysql.connector import Error
+
+load_dotenv()
+
+
+def create_connection(host_name, user_name, user_password, database_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=database_name,
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+connection = create_connection(
+    "localhost", os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), "robocritic"
+)
---
+++ helper_functions/query_functions.py
@@ -0,0 +1,12 @@
+from mysql.connector import Error
+
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
