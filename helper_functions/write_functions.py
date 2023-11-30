2ab3b1a66305d7acb67d323abb51589c3689c367
Nate Pruitt <nlpruitt@gmail.com>
2023-11-30 09:48:30 -0600

    Helper functions to write pro and con text to database

--- helper_functions/write_functions.py
+++ helper_functions/write_functions.py
@@ -23,3 +23,37 @@ def create_review(connection, review):
         connection.commit()
     except Error as e:
         print(f"The error '{e}' occurred")
+
+
+def write_review_pros_to_db(connection, list_of_pros):
+    """
+    Uses a db connection to write a list of pros to the database.
+
+    list_of_pros: List of tuples with the first element being the review_id and the second element being the text.
+    """
+    # Accepts a db connection and a list of pros.
+    # list_of_pros: List of strings
+    create_pros_query = "INSERT INTO review_pro (review_id, text) VALUES (%s, %s)"
+    cursor = connection.cursor()
+    try:
+        cursor.executemany(create_pros_query, list_of_pros)
+        connection.commit()
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def write_review_cons_to_db(connection, list_of_cons):
+    """
+    Uses a db connection to write a list of cons to the database.
+
+    list_of_cons: List of tuples with the first element being the review_id and the second element being the text.
+    """
+    # Accepts a db connection and a list of cons.
+    # list_of_cons: List of strings
+    create_cons_query = "INSERT INTO review_con (review_id, text) VALUES (%s, %s)"
+    cursor = connection.cursor()
+    try:
+        cursor.executemany(create_cons_query, list_of_cons)
+        connection.commit()
+    except Error as e:
+        print(f"The error '{e}' occurred")
