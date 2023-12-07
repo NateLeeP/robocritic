827bcc0c0332ccac74751a4fbc7b9a7fd34ffc75
Nate Pruitt <nlpruitt@gmail.com>
2023-12-07 07:46:53 -0600

    Read game from DB by title

--- helper_functions/query_functions.py
+++ helper_functions/query_functions.py
@@ -10,3 +10,35 @@ def execute_read_query(connection, query):
         return result
     except Error as e:
         print(f"The error '{e}' occurred")
+
+
+def get_publisher_by_id(connection, id):
+    """
+    Return the publisher with given id
+    """
+    cursor = connection.cursor()
+    query = "SELECT * FROM publisher WHERE id = %s"
+    publisher = None
+    try:
+        cursor.execute(query, (id,))
+        publisher = cursor.fetchone()
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return publisher
+
+
+def get_game_by_title(connection, title):
+    """
+    Return the game with given title
+    """
+    cursor = connection.cursor()
+    query = "SELECT * FROM game WHERE title = %s"
+    game = None
+    try:
+        cursor.execute(query, (title,))
+        game = cursor.fetchone()
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return game
