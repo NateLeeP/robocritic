c6f70e088f623ed480169c62df25e83b52ba69f5
Nate Pruitt <nlpruitt@gmail.com>
2023-12-21 08:04:40 -0600

    Refactor function to return release date from first API call

--- helper_functions/igdb_service.py
+++ helper_functions/igdb_service.py
@@ -20,27 +20,55 @@ class IGDBService:
         Returns the release date as a datetime date object
         from the associated game title.
         """
-        release_date_id = self.__get_release_date_id_by_title(title)
-        release_date_epoch = requests.post(
-            f"{self.base_url}/release_dates",
-            headers=self.headers,
-            data=f"fields date; where id = {release_date_id};",
-        )
+        try:
+            release_date_id = self.get_release_date_id_by_title(title)
+        except ValueError as e:
+            print(e)
+            return None
+        try:
+            request_data = f"fields date; where id = {release_date_id};"
+            response = requests.post(
+                f"{self.base_url}/release_dates",
+                headers=self.headers,
+                data=request_data,
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            print(f"Request failed with data: {request_data}")
+            return None
 
-        unix_time = release_date_epoch.json()[0]["date"]
+        if not response.json():
+            raise ValueError(f"No release date found with title '{title}'")
+        unix_time = response.json()[0]["date"]
         release_date = datetime.utcfromtimestamp(unix_time)
         return release_date.date()
 
-    def __get_release_date_id_by_title(self, title):
+    def get_release_date_id_by_title(self, title):
         """
         Returns the IGDB release data id from the associated game title.
         Returns the last release date in array.
         Returning the first release date resulted in beta / demo release dates
         being returned.
         """
-        release_date_id = requests.post(
-            f"{self.base_url}/games",
-            headers=self.headers,
-            data=f'fields name, genres, platforms, release_dates; where name = "{title}";',
-        )
-        return release_date_id.json()[0]["release_dates"][-1]
+        try:
+            request_data = (
+                f'search "{title}"; fields name, genres, platforms, first_release_date;'
+            )
+            response = requests.post(
+                f"{self.base_url}/games",
+                headers=self.headers,
+                data=request_data,
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            print(f"Request failed with data: {request_data}")
+            return None
+        if not response.json():
+            # Will raise an error for empty response
+            raise ValueError(f"No game found with title '{title}'")
+        game = response.json()[0]
+        release_date_unix = game["first_release_date"]
+        release_date = datetime.utcfromtimestamp(release_date_unix).date()
+        return release_date
