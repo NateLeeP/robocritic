4451b78313f89ca3d7c98906a8c73a8f1cd2aba4
Nate Pruitt <nlpruitt@gmail.com>
2024-01-15 07:43:41 -0600

    Build service classes for read / write operations on the DynamoDB tables

--- helper_functions/query_functions.py
+++ helper_functions/query_functions.py
@@ -1,4 +1,123 @@
 from mysql.connector import Error
+import boto3
+import botocore
+
+
+class GameReleaseDate:
+    """
+    Class for querying the GameRleaseDate DynamoDB table.
+    """
+
+    def __init__(self):
+        self.dynamodb_client = boto3.client("dynamodb")
+
+    def get_recent_games(self, limit=10):
+        """Return the most recent games. Limit return with passed 'limit' argument"""
+        response = self.dynamodb_client.query(
+            TableName="GamesReleaseDate",
+            KeyConditionExpression="SortByReleaseDate = :val",
+            ExpressionAttributeValues={":val": {"N": "1"}},
+            ScanIndexForward=False,
+            Limit=limit,
+        )
+        return response["Items"]
+
+    def write_item(self, release_date_game_title):
+        """
+        Writes a new item to the GameRleaseDate DynamoDB table.
+
+        release_date_game_title: a string with format 'YYYY-MM-DD_{game_title}'
+        Returns HTTP response object returned by AWS.
+        """
+        try:
+            response = self.dynamodb_client.put_item(
+                TableName="GamesReleaseDate",
+                Item={
+                    "SortByReleaseDate": {"N": "1"},
+                    "ReleaseDate_GameTitle": {"S": release_date_game_title},
+                },
+                ConditionExpression="attribute_not_exists(SortByReleaseDate)",
+            )
+            return response
+        except botocore.exceptions.ClientError as error:
+            if error.response["Error"]["Code"] == "ConditionalCheckFailedException":
+                print("Item already exists")
+            return error.response
+
+
+class Reviews:
+    """
+    Class for querying the Reviews DynamoDB table.
+    """
+
+    def __init__(self):
+        self.dynamodb_client = boto3.client("dynamodb")
+
+    def get_all_reviews_by_game_title(self, game_title):
+        """
+        Return all reviews for a given game title.
+        """
+        response = self.dynamodb_client.query(
+            TableName="Reviews",
+            KeyConditionExpression="GameTitle = :val",
+            ExpressionAttributeValues={":val": {"S": game_title}},
+            ScanIndexForward=False,
+        )
+        return response["Items"]
+
+    def create_reviews_table_item(
+        self,
+        game_title,
+        review_publisher_name,
+        release_date,
+        list_of_pros,
+        list_of_cons,
+        roboscore,
+    ):
+        """
+        Create an DynamoDB table item for insertion into 'Reviews' table.
+        Lists should be a list of dictionary with key value pair of
+        'S' for string then the string value.
+        """
+        item = {
+            "GameTitle": {"S": game_title},
+            "ReviewPublisherName": {"S": review_publisher_name},
+            "GameReleaseDate": {"S": release_date},
+            "Pros": {"L": list_of_pros},
+            "Cons": {"L": list_of_cons},
+            "RoboScore": {"N": roboscore},
+        }
+        return item
+
+    def write_item(
+        self,
+        game_title,
+        review_publisher_name,
+        game_release_date,
+        list_of_pros,
+        list_of_cons,
+        roboscore,
+    ):
+        """
+        Writes a new item to the Reviews DynamoDB table.
+        Returns HTTP response object returned by AWS.
+        """
+        try:
+            response = self.dynamodb_client.put_item(
+                TableName="Reviews",
+                Item=self.create_reviews_table_item(
+                    game_title,
+                    review_publisher_name,
+                    game_release_date,
+                    list(map(lambda x: {"S": x}, list_of_pros)),
+                    list(map(lambda x: {"S": x}, list_of_cons)),
+                    str(roboscore),
+                ),
+            )
+            return response
+        except botocore.exceptions.ClientError as error:
+            print(f"The error '{error.response['Error']['Message']}' occurred")
+            return error.response
 
 
 def execute_read_query(connection, query):
