31f5cc304e3a53b83523729d2434f889a34c061d
Nate Pruitt <nlpruitt@gmail.com>
2024-01-18 08:12:14 -0600

    Add function to get release date from metacritic

--- helper_functions/scraping_utils.py
+++ helper_functions/scraping_utils.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 ign_latest_reviews_url = "https://www.ign.com/reviews/games"
 header_mapping = {"User-Agent": "Mac Firefox"}
@@ -40,7 +41,7 @@ def get_latest_reviews_from_ign():
 
     # filter out board games
     video_game_reviews = list(
-        filter(lambda x: "Board Game" not in x["title"], mapped_latest_reviews)
+        filter(lambda x: "board-game" not in x["href"], mapped_latest_reviews)
     )
 
     return video_game_reviews
@@ -66,3 +67,39 @@ def get_game_review_content_from_ign(url):
     articles = soup.find_all("p")
     content = "".join(list(map(lambda x: x.get_text(), articles)))
     return content
+
+
+def get_game_release_date_from_metacritic(game_title):
+    """
+    Accepts a game title and returns a datetime 'date' object. If error is thrown, return none.
+
+    Returns:
+        "date" of type 'datetime.date'.
+    """
+    try:
+        game_title = (
+            game_title.lower().replace(":", "").replace("'", "").replace(" ", "-")
+        )
+        response = requests.get(
+            f"https://www.metacritic.com/game/{game_title}", headers=header_mapping
+        )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        raise requests.exceptions.HTTPError(
+            f"Request failed with url: https://www.metacritic.com/browse/games/{game_title}"
+        )
+    soup = BeautifulSoup(response.content, "html.parser")
+    try:
+        release_date = soup.find(
+            lambda tag: tag.name == "span"
+            and tag.has_attr("class")
+            and tag["class"] == ["u-text-uppercase"]
+        ).text
+        release_date = datetime.strptime(release_date, "%b %d, %Y").date()
+        return release_date
+    except Exception as e:
+        print(e)
+        raise ValueError(
+            f"Failed to parse html content from metacritic for game title: {game_title}"
+        )
