640a7e47da8b8b612deabbb077861a79b2eb2b39
Nate Pruitt <nlpruitt@gmail.com>
2024-02-19 10:42:51 -0600

    Create Robocritic banner

--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -2,14 +2,13 @@ import Link from 'next/link'
 
 
 async function getGames() {
-    const response = await fetch("https://g5ql747n3i.execute-api.us-east-1.amazonaws.com/games")
+    const response = await fetch("https://g5ql747n3i.execute-api.us-east-1.amazonaws.com/games", { cache: 'no-store' })
 
     if (!response.ok) {
         throw new Error('Failed to fetch reviews')
     }
 
     const games = response.json()
-    console.log(games);
     return games
 }
 
@@ -17,11 +16,11 @@ export default async function Reviews() {
     const games = await getGames();
     return (
         <div>
-            <h1>Reviews</h1>
+            <h1>Games</h1>
             {
                 games.map((game, index) => (
                     <div key={index}>
-                        <Link href="/game/prince-of-persia-the-lost-crown?title=Test">Link</Link>
+                        <Link href={`/game/${game.url_path}`}>Click me!</Link>
                         <h2>{game.game_title}</h2>
                         <img src={game.game_art_url} />
                         <p>{game.release_date}</p>
