50fc1f7f2e14c87129791db7eb33a6913fbb9016
Nate Pruitt <nlpruitt@gmail.com>
2024-06-11 07:30:54 -0500

    Improve mobile design

--- frontend/components/Games.js
+++ frontend/components/Games.js
@@ -12,10 +12,10 @@ export default function Games({ games }) {
     ];
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-5 gap-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 sm:grid-cols-3 gap-4'>
             {
                 games.map((game, index) => (
-                    <div key={index} className='p-8 flex flex-col'>
+                    <div key={index} className='p-8 flex flex-col items-center sm:items-start'>
                         <Link href={`/game/${game.url_path}`}><img src={game.game_art_url} alt={`Art for ${game.game_title}`} className='w-36 h-36 rounded-2xl hover:shadow-2xl' /></Link>
                         <h2 className="text-wrap font-semibold font-serif text-l text-stone-300">{game.game_title}</h2>
                         <p className='font-serif text-sm text-neutral-300'>Release Date: {game.release_date}</p>
