b1c72a278fea38249e7ef5a7c197dc756daaf9b3
Nate Pruitt <nlpruitt@gmail.com>
2024-06-13 07:39:02 -0500

    Replace <Link> components with <a> tag

--- frontend/components/Games.js
+++ frontend/components/Games.js
@@ -16,10 +16,10 @@ export default function Games({ games }) {
             {
                 games.map((game, index) => (
                     <div key={index} className='p-8 flex flex-col items-center sm:items-start'>
-                        <Link href={`/game/${game.url_path}`}><img src={game.game_art_url} alt={`Art for ${game.game_title}`} className='w-36 h-36 rounded-2xl hover:shadow-2xl' /></Link>
+                        <a href={`/game/${game.url_path}`}><img src={game.game_art_url} alt={`Art for ${game.game_title}`} className='w-36 h-36 rounded-2xl hover:shadow-2xl' /></a>
                         <h2 className="text-wrap font-semibold font-serif text-l text-stone-300">{game.game_title}</h2>
                         <p className='font-serif text-sm text-neutral-300'>Release Date: {game.release_date}</p>
-                        <Link className="font-serif text-stone-300 italic underline hover:font-bold" href={`/game/${game.url_path}`}>Read Reviews</Link>
+                        <a className="font-serif text-stone-300 italic underline hover:font-bold" href={`/game/${game.url_path}`}>Read Reviews</a>
                     </div>
                 ))
             }
--- frontend/components/banner.js
+++ frontend/components/banner.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 export default function Banner({ }) {
 
     return (
         <div className="bg-slate-600">
-            <Link className="flex justify-center" href="/"><Image src="/robot_logo.png" alt="robocritic-log" className='bg-transparent' width="100" height="100" /></Link>
-            <Link href="/"><h1 className="text-2xl font-mono text-center font-bold text-stone-300">RoboCritic</h1></Link>
+            <a className="flex justify-center" href="/"><Image src="/robot_logo.png" alt="robocritic-log" className='bg-transparent' width="100" height="100" /></a>
+            <a href="/"><h1 className="text-2xl font-mono text-center font-bold text-stone-300">RoboCritic</h1></a>
         </div>
     );
 }
