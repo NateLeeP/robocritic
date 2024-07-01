f0feb5508774318fb1197fc43fa12483c7f6c36e
Nate P <nlpruitt@gmail.com>
2024-07-01 18:49:07 -0500

    Margin between title and image. Bold the pros and cons headers

--- frontend/components/Game.js
+++ frontend/components/Game.js
@@ -3,9 +3,9 @@ import Review from './Review';
 export default function Game({ gameArtURL, gameTitle, reviews }) {
 
     return (
-        <div className="text-stone-300">
-            <h1 className="font-mono italic text-3xl text-stone-300">{gameTitle}</h1>
-            <img src={gameArtURL} alt={gameTitle} className='w-36 h-36' />
+        <div className="pl-10 pr-10 text-stone-300">
+            <h1 className="font-mono text-6xl text-stone-300">{gameTitle}</h1>
+            <img src={gameArtURL} alt={gameTitle} className='w-36 h-36 mb-2 mt-5' />
             <Review review={reviews[0]} />
         </div>
     )
