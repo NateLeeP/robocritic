edc315718516e9ee87e50496d6c8f3fdf5c0429e
Nate P <nlpruitt@gmail.com>
2024-08-19 09:00:31 -0500

    PC Gamer ETL functions and main executable

--- helper_functions/igdb_service.py
+++ helper_functions/igdb_service.py
@@ -17,36 +17,7 @@ class IGDBService:
             "Client-ID": os.environ["IGDB_CLIENT_ID"],
         }
 
-    def get_game_release_date_by_title(self, title):
-        """
-        Returns the release date as a datetime date object
-        from the associated game title.
-        """
-        try:
-            release_date_id = self.get_release_date_id_by_title(title)
-        except ValueError as e:
-            logger.error(e)
-            return None
-        try:
-            request_data = f"fields date; where id = {release_date_id};"
-            response = requests.post(
-                f"{self.base_url}/release_dates",
-                headers=self.headers,
-                data=request_data,
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            logger.error(e)
-            logger.error(f"HTTP Request failed with data: {request_data}")
-            return None
-
-        if not response.json():
-            raise ValueError(f"No release date found with title '{title}'")
-        response.json().sort(key=lambda x: x["id"], reverse=True)
-        unix_time = response.json()[0]["date"]
-        release_date = datetime.utcfromtimestamp(unix_time)
-        return release_date.date()
-
+    
     def get_first_release_date_by_title(self, title):
         """
         Returns the IGDB release data id from the associated game title.
