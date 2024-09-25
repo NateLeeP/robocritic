6a045807b4bd33f54629d7a3082dc38c47169690
NateLeeP <nlpruitt@gmail.com>
2024-09-25 07:15:40 -0500

    Views with styling for home page

--- app/robocritic/views.py
+++ app/robocritic/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Game
 def index(request):
-    return render(request, 'robocritic/index.html')
+    games = Game.objects.all()
+
+    return render(request, 'robocritic/index.html', {'games': games})
 
