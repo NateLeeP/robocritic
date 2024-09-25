472a7d83ca6498573dd15691fc1793ad41762d64
Nate Pruitt <nlpruitt@gmail.com>
2024-09-25 18:06:52 -0500

    Clean up style on review. Link from game page to review page.

--- app/robocritic/views.py
+++ app/robocritic/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Game, Review
+
+
 def index(request):
     games = Game.objects.all()
 
-    return render(request, 'robocritic/index.html', {'games': games})
+    return render(request, "robocritic/index.html", {"games": games})
+
 
 def review(request, game_id):
     reviews = Review.objects.filter(game__id=game_id)
-    return render(request, 'robocritic/review.html', {'reviews': reviews})
-
+    game = Game.objects.get(id=game_id)
+    return render(request, "robocritic/review.html", {"reviews": reviews, "game": game})
