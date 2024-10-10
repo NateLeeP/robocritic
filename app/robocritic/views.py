b054d636fbcfff1600ded6c8f9107dd3b7e2769b
NateLeeP <nlpruitt@gmail.com>
2024-10-10 07:30:17 -0500

    raise 404 error if game not found

--- app/robocritic/views.py
+++ app/robocritic/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Game, Review
 
 
 def index(request):
     games = Game.objects.all()
-
     return render(request, "robocritic/index.html", {"games": games})
 
 
 def review(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
     reviews = Review.objects.filter(game__id=game_id)
-    game = Game.objects.get(id=game_id)
     return render(request, "robocritic/review.html", {"reviews": reviews, "game": game})
